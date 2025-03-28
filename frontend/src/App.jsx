36756f69212a197138a235628a91712397782b75
Theekshan Chathuranga <109914468+TheekshanaChathuranga@users.noreply.github.com>
2025-03-28 22:21:07 +0530

    Make modification home page
    Add two section Room booking, view room booking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import RoomsList from "./pages/RoomsList";
 import AddRoom from "./pages/AddRoom";
 import Profile from "./pages/Profile";
+import RoomDetails from "./pages/RoomDetails";
+import BookingPage from "./pages/BookingPage";
 
 export default function App() {
   return (
@@ -14,6 +16,8 @@ export default function App() {
         <Route path="/rooms" element={<RoomsList />} />
         <Route path="/add-room" element={<AddRoom />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/rooms/:id" element={<RoomDetails />} />
+        <Route path="/booking" element={<BookingPage />} />
       </Routes>
     </Router>
   );
